981f6811d68fed9780a0d05bcf55ad9a78dbc8a7
Rahix <rahix@rahix.de>
2018-09-04 16:57:12 +0200

    Add coloring

--- gen_wires.py
+++ gen_wires.py
@@ -10,21 +10,21 @@ def main() -> None:
         first = w["path_a"][0]
         points_anim = " L ".join(f"{x} {y}" for x, y in w["path_a"])
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
-        print('<g class="wire-group">')
+        print(f'<g class="wire-group wire-color-{w["color"]}">')
         print(f'    <path class="wire anim" d="M {points_anim}" />')
         print(f'    <path class="wire" d="M {points_static}" />')
         print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="6" class="wire" />')
-        print('</g>')
+        print(f'</g>')
     print("{% endmacro js %}")
     print("")
     print("{% macro nojs() %}")
     for w in data:
         first = w["path_b"][0]
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
-        print('<g class="wire-group">')
+        print(f'<g class="wire-group wire-color-{w["color"]}">')
         print(f'    <path class="wire" d="M {points_static}" />')
         print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="6" class="wire" />')
-        print('</g>')
+        print(f'</g>')
     print("{% endmacro nojs %}")
 
 
