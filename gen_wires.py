f1639bdcc9122828c2b4beba4f000b3922ec2220
Rahix <rahix@rahix.de>
2018-09-04 17:08:37 +0200

    Width and radius

--- gen_wires.py
+++ gen_wires.py
@@ -1,29 +1,42 @@
 import json
 
+def style(width: float) -> None:
+    print(f'<defs>')
+    print(f'    <style>')
+    print(f'        g.wire-group .wire {{')
+    print(f'            stroke-width: {width}px;')
+    print(f'        }}')
+    print(f'    </style>')
+    print(f'</defs>')
 
 def main() -> None:
     with open("wires.json") as f:
         data = json.load(f)
 
+    width = data["width"]
+    radius = data["radius"]
+
     print("{% macro js() %}")
-    for w in data:
+    style(width)
+    for w in data["data"]:
         first = w["path_a"][0]
         points_anim = " L ".join(f"{x} {y}" for x, y in w["path_a"])
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
         print(f'<g class="wire-group wire-color-{w["color"]}">')
         print(f'    <path class="wire anim" d="M {points_anim}" />')
         print(f'    <path class="wire" d="M {points_static}" />')
-        print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="6" class="wire" />')
+        print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="{radius}" class="wire" />')
         print(f'</g>')
     print("{% endmacro js %}")
     print("")
     print("{% macro nojs() %}")
-    for w in data:
+    style(width)
+    for w in data["data"]:
         first = w["path_b"][0]
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
         print(f'<g class="wire-group wire-color-{w["color"]}">')
         print(f'    <path class="wire" d="M {points_static}" />')
-        print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="6" class="wire" />')
+        print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="{radius}" class="wire" />')
         print(f'</g>')
     print("{% endmacro nojs %}")
 
