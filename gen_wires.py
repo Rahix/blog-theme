8299b56423991e62de119ec622f41e0d2f8961ee
Rahix <rahix@rahix.de>
2018-09-04 17:53:09 +0200

    Add narrow wires

--- gen_wires.py
+++ gen_wires.py
@@ -1,14 +1,20 @@
 import sys
 import json
+import random
 
-def style(width: float) -> None:
+def style(pre: str, width: float) -> None:
     print(f'<defs>')
     print(f'    <style>')
-    print(f'        g.wire-group .wire {{')
+    print(f'        g.wire-group.p{pre} .wire {{')
     print(f'            stroke-width: {width}px;')
     print(f'        }}')
     print(f'    </style>')
     print(f'</defs>')
+
+
+def prefix() -> str:
+    return str(random.randint(10000, 100000))
+
 
 def main() -> None:
     if len(sys.argv) != 2:
@@ -21,12 +27,13 @@ def main() -> None:
     radius = data["radius"]
 
     print("{% macro js() %}")
-    style(width)
+    pre = prefix()
+    style(pre, width)
     for w in data["data"]:
         first = w["path_a"][0]
         points_anim = " L ".join(f"{x} {y}" for x, y in w["path_a"])
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
-        print(f'<g class="wire-group wire-color-{w["color"]}" data-wire-duration="{w["duration"]}">')
+        print(f'<g class="wire-group wire-color-{w["color"]} p{pre}" data-wire-duration="{w["duration"]}">')
         print(f'    <path class="wire anim" d="M {points_anim}" />')
         print(f'    <path class="wire" d="M {points_static}" />')
         print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="{radius}" class="wire" />')
@@ -34,11 +41,12 @@ def main() -> None:
     print("{% endmacro js %}")
     print("")
     print("{% macro nojs() %}")
-    style(width)
+    pre = prefix()
+    style(pre, width)
     for w in data["data"]:
         first = w["path_b"][0]
         points_static = " L ".join(f"{x} {y}" for x, y in w["path_b"])
-        print(f'<g class="wire-group wire-color-{w["color"]}">')
+        print(f'<g class="wire-group wire-color-{w["color"]} p{pre}">')
         print(f'    <path class="wire" d="M {points_static}" />')
         print(f'    <circle cx="{first[0]}" cy="{first[1]}" r="{radius}" class="wire" />')
         print(f'</g>')
